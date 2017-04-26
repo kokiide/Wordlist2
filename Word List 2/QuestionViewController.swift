fca16e6ef0f83c9cde6cf2b78fcab6054e350b46
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-04-26 16:11:42 +0900

    Storyboard fixed! halfway through ViewController Code...

---
+++ Word List 2/QuestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuestionViewController.swift
+//  Word List 2
+//
+//  Created by Koki Ide on 2017/04/26.
+//  Copyright © 2017 kokiide. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
